49a10593aab8c70a5b4d0266da898dde80646055
Pierre-Jean Risticoni <risticonipj@gmail.com>
2024-07-08 22:01:32 +0200

    Mise en place des d'une barre de navigation pour les galeries

--- src/components/DropDownGallery/DropDownGaller.jsx
+++ src/components/DropDownGallery/DropDownGaller.jsx
@@ -2,17 +2,24 @@ import PropTypes from "prop-types";
 
 function DropDownGallery({ pages, onSelect, selectedIndex }) {
     return (
-        <select
-            value={selectedIndex}
-            onChange={(e) => onSelect(Number(e.target.value))}
-             className="dropDownGallery"
-        >
-            {pages.map((page, index) => (
-                <option key={index} value={index} className="dropDownGallery__item">
-                    {page.title}
-                </option>
-            ))}
-        </select>
+        <div className="dropDownGalleryContainer">
+            <select
+                value={selectedIndex}
+                onChange={(e) => onSelect(Number(e.target.value))}
+                className="dropDownGallery"
+            >
+                {pages.map((page, index) => (
+                    <option
+                        key={index}
+                        value={index}
+                        className="dropDownGallery__item"
+                    >
+                        {page.title}
+                    </option>
+                ))}
+            </select>
+            <div className="dropDownGallery__arrow"></div>
+        </div>
     );
 }
 
